cc005b3f974b2799ce13c9e926f865f678015b90
CodeRacket <outiswebsolutions@gmail.com>
2025-05-19 19:21:44 -0400

    Added first MVP

---
+++ app/__init__.py
@@ -0,0 +1,36 @@
+##########
+# Memo: Script thay handles Flask setup with SQLAlchemy, Flask_login, and also defines how a user is loaded for a session  
+###########
+from flask import Flask
+from flask_login import LoginManager
+from .models import db, User
+from dotenv import load_dotenv
+import os
+load_dotenv()
+login_manager = LoginManager()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+    # initialize extensions
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    # Configure Login Behaviour
+    login_manager.login_view = 'main.login'
+    login_manager.login_message_category = 'info'
+
+    # User Loaded Callback: Flask-Login needs this
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+
+    # Register the Routes!
+    from app.routes import main as main_blueprint # Required to Load @main.route
+    app.register_blueprint(main_blueprint)
+    
+    return app
+
+
---
+++ app/routes.py
@@ -0,0 +1,80 @@
+#######
+#Date: 2025/05/14
+# Filename: routes.py
+# Summary: routes.py    Gets form data, looks up the user, and calls user.check_password(...)
+#######
+
+# Flask and DB imports for easy template rendering & handling visitors and members. 
+from flask import Blueprint, render_template, redirect, url_for, flash, request
+from flask_login import login_user, current_user, logout_user, login_required
+from app.models import db, User
+from app.forms import RegistrationForm, LoginForm
+from flask import current_app as app
+
+# Create the Blueprint
+main = Blueprint('main', __name__)
+
+# Defining Root Route
+@main.route('/')
+def home():
+    return render_template('home.html')
+
+# Define Registration Route
+@main.route('/register', methods=['GET', 'POST'])
+# registration function
+def register():
+    # Only Allow Access to Dashboard by Authenticated Users
+    if current_user.is_authenticated:
+        return redirect(url_for('main.dashboard'))
+
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        existing_user = User.query.filter_by(email=form.email.data).first()
+        # Check if registration email already exists in DB
+        if existing_user:
+            flash("Email is already Registered, try loggin in or using a different email", 'danger')
+            return render_template('register.html', form=form)
+        # Create new user
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Account created! You may now Login.', 'success')
+        return redirect(url_for('main.login'))
+
+    return render_template('register.html', form=form)
+
+# Define Login  Route
+@main.route('/login', methods=["GET", "POST"])
+def login():
+    # check if user is authenticated
+    if current_user.is_authenticated:
+        print(f"User Exists: {user}")
+        print(f"Authenticated: {current_user.is_authenticated}")
+        return redirect(url_for('main.dashboard'))
+
+    # Initialize the LoginForm
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user)
+            flash(f'Successfully Logged in as: {current_user.username} ', 'success')
+            return redirect(url_for('main.dashboard'))
+        else:
+            flash('Login Failed. Check email/password', 'danger')
+    return render_template('login.html', form=form)
+
+# Define DashBoard Route
+@main.route('/dashboard')
+@login_required
+def dashboard():
+    print(f"User Authenticated: {current_user.is_authenticated}, ID: {current_user.id}")
+    return render_template('dashboard.html', name=current_user.username)
+
+# Define Logout Route
+@main.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.login'))
