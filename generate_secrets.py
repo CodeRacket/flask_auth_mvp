b5491fa147cf8e93977321e02fa15a956bb56912
Nathaniel Andreano <outiswebsolutions@gmail.com>
2025-08-26 05:43:35 -0400

    Update generate_secrets.py
    minor fixes

--- generate_secrets.py
+++ generate_secrets.py
@@ -4,10 +4,10 @@ def generate_env_secrets():
     keys = {}
 
     with open('.env', 'w') as f:
-        for key, value in key.items():
+        for key, value in keys.items():
             f.write(f"{key}={value}\n")
         
         print(".env file created with secure secrets")
-if __name__ in '__main__':
+if __name__ == '__main__':
     generate_env_secrets()
 
