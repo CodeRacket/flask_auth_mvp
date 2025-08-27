a9d7a1f62bddb2cc4000bcf2d6ea9c82876f6024
Mobius Jean <mobius@DevelopmentOS.Mobius-Wlan>
2025-08-27 12:51:42 -0400

    Script to initialize first run of Flask auth app

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
-from app import app,db
-
+# First time run only, creates the database model
+# If PostgreSQL Permission errors: run  'docker compose down -v && docker compose up --build'
+from app import create_app 
+from app.models import db
+app = create_app()
 with app.app_context():
     db.create_all()
-    print("Database tables created.")
-
