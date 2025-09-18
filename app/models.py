4b4c3789044b903d8e93b223d6534bc87fd5bd9a
Mobius Jean <mobius@DevelopmentOS.Mobius-Wlan>
2025-09-18 01:42:29 -0400

    lorem ipsum dolor sit amet

--- app/models.py
+++ app/models.py
@@ -18,10 +18,12 @@ db = SQLAlchemy()
 class User(db.Model, UserMixin):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
+    # unique imposes constraints on duplicate account user/email data
     username = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     # below—Complains the string exceeded buffer, 128 bytes or chars
     # password_hash = db.Column(db.String(128), nullable=False)
+    # werkzeug hashing applied to protect user data
     password_hash = db.Column(db.Text, nullable=False)
 
     # Hashes the Password before stroing into DB
